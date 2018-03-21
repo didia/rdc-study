10b82ffcc07f5a5dcbcf5ef98a7ed352ea4d6ba9
Aristote Diasonama <aristotediasonama@yahoo.fr>
2018-03-20 23:25:12 -0400

    Add sass support

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Guide d’études à l’étranger pour étudiants congolais',
   },
-  plugins: ['gatsby-plugin-react-helmet']
-};
+  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-react-helmet'],
+}
