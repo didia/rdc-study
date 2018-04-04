1e824061b89b353ab3f3ac664e00e8e47ce15fdd
Aristote Diasonama <aristotediasonama@yahoo.fr>
2018-04-04 00:39:57 -0400

    Add support for Sentry

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,50 @@
 /* eslint-env node */
+const plugins = [
+  'gatsby-plugin-react-helmet',
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-sharp',
+  'gatsby-plugin-react-next',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'images',
+      path: `${__dirname}/src/assets/images`
+    }
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/data/guides`,
+      name: 'guides'
+    }
+  },
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 590
+          }
+        },
+        'gatsby-remark-prismjs',
+        'gatsby-remark-copy-linked-files',
+        'gatsby-remark-smartypants'
+      ]
+    }
+  }
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push({
+    resolve: 'gatsby-plugin-sentry',
+    options: {
+      dsn: process.env.SENTRY_DSN_URL
+    }
+  });
+}
 
 module.exports = {
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-react-next',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/assets/images`
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/data/guides`,
-        name: 'guides'
-      }
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590
-            }
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants'
-        ]
-      }
-    }
-  ]
+  plugins
 };
