7a222744f184184a0f6ca71d775107be4fa812f3
Aristote Diasonama <aristotediasonama@yahoo.fr>
2019-05-17 18:03:11 -0400

    Add canonical url to sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,9 +12,40 @@ const plugins = [
   netlifyCmsPaths,
   'gatsby-transformer-sharp',
   'gatsby-plugin-sharp',
-  'gatsby-plugin-sitemap',
   'gatsby-plugin-remove-trailing-slashes',
   'gatsby-plugin-catch-links',
+  {
+    resolve: 'gatsby-plugin-sitemap',
+    options: {
+      query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage(
+              filter: {
+                path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+              }
+            ) {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+          }
+        `,
+      output: '/sitemap.xml',
+      serialize: ({site, allSitePage}) =>
+        allSitePage.edges.map(edge => ({
+          url: `${site.siteMetadata.siteUrl + edge.node.path}/`,
+          changefreq: 'daily',
+          priority: 0.7
+        }))
+    }
+  },
   {
     resolve: 'gatsby-plugin-sass',
     options: {
