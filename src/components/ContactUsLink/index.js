3ae5f3bdd62a28e9227ea815c3355583e8fc0f60
Aristote Diasonama <aristotediasonama@yahoo.fr>
2019-06-23 05:18:49 -0400

    Use anchor instead of link in contact us button

--- src/components/ContactUsLink/index.js
+++ src/components/ContactUsLink/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 import T from 'prop-types';
-import {Link} from 'gatsby';
 
 const ContactUsButton = ({className}) => (
   <FormattedMessage id="shared.contact-us-text">
     {text => (
-      <Link to={`${window.location.pathname}#contact`} className={className}>
+      <a href="#contact" className={className}>
         {text}
-      </Link>
+      </a>
     )}
   </FormattedMessage>
 );
