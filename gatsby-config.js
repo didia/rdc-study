fc4e83d5263735fd5f57f58da2469f3e0806d706
Aristote Diasonama <aristotediasonama@yahoo.fr>
2019-10-11 23:16:21 -0400

    Update netlify cms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ const plugins = [
   'gatsby-plugin-sharp',
   'gatsby-plugin-remove-trailing-slashes',
   'gatsby-plugin-catch-links',
+  'gatsby-plugin-netlify-cms',
   {
     resolve: 'gatsby-plugin-sitemap',
     options: {
