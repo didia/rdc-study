2defb2c0660fd107d58934d2a4b15428ba629e2a
Aristote Diasonama <aristotediasonama@yahoo.fr>
2020-06-06 15:49:29 -0400

    wip

--- src/components/AssistanceForm/index.js
+++ src/components/AssistanceForm/index.js
@@ -16,36 +16,38 @@ const AboutCandidateStep = () => {
       <h2 className={styles['step-title']}>{intl.formatMessage({id: 'assistance-form.about-candidate.title'})}</h2>
       <p>{intl.formatMessage({id: 'assistance-form.about-candidate.description'})}</p>
 
-      <div className="field">
-        <label htmlFor="firstName" className={styles.label}>
-          {intl.formatMessage({id: 'assistance-form.about-candidate.first-name'})}
-        </label>
+      <div className={styles.fields}>
+        <div className="field">
+          <label htmlFor="firstName" className={styles.label}>
+            {intl.formatMessage({id: 'assistance-form.about-candidate.first-name'})}
+          </label>
 
-        <input type="text" name="firstName" className={styles.input} />
-      </div>
+          <input type="text" name="firstName" className={styles.input} />
+        </div>
 
-      <div className="field">
-        <label htmlFor="lastName" className={styles.label}>
-          {intl.formatMessage({id: 'assistance-form.about-candidate.last-name'})}
-        </label>
+        <div className="field">
+          <label htmlFor="lastName" className={styles.label}>
+            {intl.formatMessage({id: 'assistance-form.about-candidate.last-name'})}
+          </label>
 
-        <input type="text" name="lastName" className={styles.input} />
-      </div>
+          <input type="text" name="lastName" className={styles.input} />
+        </div>
 
-      <div className="field">
-        <label htmlFor="email" className={styles.label}>
-          {intl.formatMessage({id: 'assistance-form.about-candidate.email'})}
-        </label>
+        <div className="field">
+          <label htmlFor="email" className={styles.label}>
+            {intl.formatMessage({id: 'assistance-form.about-candidate.email'})}
+          </label>
 
-        <input type="email" name="email" className={styles.input} />
-      </div>
+          <input type="email" name="email" className={styles.input} />
+        </div>
 
-      <div className="field">
-        <label htmlFor="originCountry" className={styles.label}>
-          {intl.formatMessage({id: 'assistance-form.about-candidate.origin-country'})}
-        </label>
+        <div className="field">
+          <label htmlFor="originCountry" className={styles.label}>
+            {intl.formatMessage({id: 'assistance-form.about-candidate.origin-country'})}
+          </label>
 
-        <CountrySelector className={styles.input} name="originCountry" />
+          <CountrySelector className={styles.input} name="originCountry" />
+        </div>
       </div>
 
       <ul className="actions">
