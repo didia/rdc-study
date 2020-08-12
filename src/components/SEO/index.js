b2d98e69c5af9304b11329e5813c002d841f4bbd
Aristote Diasonama <aristotediasonama@yahoo.fr>
2020-08-12 07:25:44 -0400

    Update SEO infos with favicon

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -81,7 +81,15 @@ const SEO = ({meta}) => {
     <Helmet>
       <title>{title}</title>
       <link rel="canonical" href={meta ? postURL : blogURL} />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4c96d7" />
+
       {/* General tags */}
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
       <meta name="description" content={description} />
       <meta name="image" content={imageURL} />
       <meta name="keywords" content={keywords} />
