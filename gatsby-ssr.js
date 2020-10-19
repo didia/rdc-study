f4544e8907401e550c57e2f40aa010a50103416c
Aristote Diasonama <aristotediasonama@yahoo.fr>
2020-10-19 09:56:20 -0400

    Remove google ads script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,21 +6,7 @@ import React from 'react';
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-const setDevOnlyScripts = ({setHeadComponents}) => {
-  if (process.env.NODE_ENV === 'production') return;
-
-  setHeadComponents([
-    <script key="ads-by-google" async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />,
-    <script
-      key="ads-by-google-script"
-      dangerouslySetInnerHTML={{
-        __html: `google_ad_test = "on"; (adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "${process.env.GATSBY_GOOGLE_AD_CLIENT}", enable_page_level_ads: true});`
-      }}
-    />
-  ]);
-};
-
-export const onRenderBody = ({setHeadComponents, setPostBodyComponents}) => {
+export const onRenderBody = ({setPostBodyComponents}) => {
   setPostBodyComponents([
     <script
       key="share-this"
@@ -42,5 +28,4 @@ export const onRenderBody = ({setHeadComponents, setPostBodyComponents}) => {
       }}
     />
   ]);
-  setDevOnlyScripts({setHeadComponents, setPostBodyComponents});
 };
