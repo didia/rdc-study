93946c927e550a74bac0e2f02ed9ff2a5831a452
philippembambi <philippembambi413@gmail.com>
2023-11-13 15:12:30 +0100

    add partener details page

---
+++ src/pages/partenership.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PartenerShip from '../components/Pages/PartenerShip';
+
+const PartenerShipPage = () => {
+    return ( <PartenerShip /> );
+}
+ 
+export default PartenerShipPage;
