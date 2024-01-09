c1d68cdde3ec529f3fae6d877c58876b6538d5ba
philippembambi <philippembambi413@gmail.com>
2024-01-09 14:50:58 +0100

    add lg responsiveness in Contact Page

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -11,7 +11,7 @@ import * as styles from "./style.module.scss";
 const Contact = () => {
     return (
       <Layout pageTitle={"Contactez-nous"}>
-        <section>
+        <section className="md:hidden">
           <div className="grid grid-cols-12 mb-8 mt-4 z-999">
             <div className="col-span-3">
               <StaticImage
@@ -29,7 +29,7 @@ const Contact = () => {
                     Lorem ispum dolor fortuna simpre cresis, out decresis vita
                     Lorem
                   </span>
-                  <StaticImage alt='phone' src="../../../images/phone.png" />
+                  <StaticImage alt="phone" src="../../../images/phone.png" />
                 </p>
               </div>
             </div>
@@ -37,19 +37,68 @@ const Contact = () => {
               <div className={styles.ellipse4}>
                 <StaticImage
                   height={300}
-                  alt='airplane'
+                  alt="airplane"
                   src="../../../images/png-clipart-black-airway-aviation-aircraft-removebg-preview6.png"
                 />
               </div>
             </div>
 
             <div>
-              <StaticImage alt='envelope' src="../../../images/envelope.png" />
+              <StaticImage alt="envelope" src="../../../images/envelope.png" />
             </div>
           </div>
         </section>
 
-        <section>
+        <section className="hidden md:block">
+          <div className="grid grid-cols-12 mb-8 mt-4 z-999">
+            <div className="col-span-3">
+              <StaticImage
+                alt="plans"
+                src="../../../images/Earth-Travel-World-Transparent-PNG.png"
+              />
+            </div>
+            <div className="col-span-7 z-10 mt-10">
+              <StaticImage
+                height={100}
+                alt="airplane"
+                className="-z-20"
+                src="../../../images/search-location2.png"
+              />
+              <div className="text-start ml-16 -mt-5">
+                <span className="text-sky-600 text-5xl font-extrabold">
+                  Contactez RDC Etudes
+                </span>{" "}
+                <p className="mt-5">
+                  <span className="text-xl">
+                    Lorem ipsum dolor sit amet consectetur. Risus id pretium
+                    massa pharetra ullamcorper. Faucibus fusce eleifend iaculis
+                    lobortis suspendisse tortor faucibus nibh. Id ut ipsum vel
+                    porttitor
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div className={styles.ellipse4}>
+                <StaticImage
+                  height={600}
+                  alt="airplane"
+                  src="../../../images/png-clipart-black-airway-aviation-aircraft-removebg-preview17.png"
+                />
+              </div>
+            </div>
+
+            <div>
+              <StaticImage
+                className="h-24 w-24"
+                alt="envelope"
+                src="../../../images/envelope.png"
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="md:hidden">
           <ValueCard
             title={"Heure de service"}
             section1={"08H-16H30"}
@@ -70,11 +119,41 @@ const Contact = () => {
           <ValueCard title={"Adresse E-mail"} section1={"info@rdcetudes.com"} />
         </section>
 
+        <section className="hidden md:block">
+          <div className="grid grid-cols-4">
+            <ValueCard
+              extraStyle={"w-[20rem]"}
+              title={"Heure de service"}
+              section1={"08H-16H30"}
+              section2={"Du Lundi au Vendredi"}
+            />
+            <ValueCard
+              extraStyle={"w-[20rem]"}
+              title={"Téléphone"}
+              section1={"+243826686661"}
+              section2={"+243826686661"}
+            />
+            <ValueCard
+              extraStyle={"w-[20rem]"}
+              title={"Bureaux"}
+              section1={
+                "Local LM3, Nouvelles galeries Présidentielles, Boulevard du 30 juin, Kinhsasa, République Démocratique du Congo"
+              }
+            />
+
+            <ValueCard
+              extraStyle={"w-[20rem]"}
+              title={"Adresse E-mail"}
+              section1={"info@rdcetudes.com"}
+            />
+          </div>
+        </section>
+
         <section>
           <ContactForm bgImg={img_overlay} />
         </section>
 
-        <section className="-mt-48">
+        <section className="">
           <GoogleMapFrame />
         </section>
       </Layout>
--- src/components/ValueCard/index.js
+++ src/components/ValueCard/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const ValueCard = ({ title, section1, section2 }) => {
+const ValueCard = ({ title, section1, section2, extraStyle }) => {
   return (
     <>
-      <div className="mx-auto mb-10 block max-w-[18rem] md:max-w-[27rem] md:h-[25rem] rounded bg-white text-left shadow-2xl border-b-8 border-sky-600">
+      <div
+        className={
+          "mx-auto mb-10 block max-w-[18rem] md:max-w-[27rem] md:h-[25rem] rounded bg-white text-left shadow-2xl border-b-8 border-sky-600 " +
+          extraStyle
+        }
+      >
         <div className="p-6">
           <h5 className="text-center mb-1 text-xl md:text-2xl font-medium leading-tight text-sky-600">
             {title}
@@ -12,7 +17,9 @@ const ValueCard = ({ title, section1, section2 }) => {
             {section1}
           </p>
 
-          <p className="mb-6 mt-6 text-base text-center">{section2}</p>
+          <p className="mb-6 mt-6 md:mt-10 md:text-lg text-base text-center">
+            {section2}
+          </p>
         </div>
       </div>
     </>
