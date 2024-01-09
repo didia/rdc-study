0951d14531fb6399d2c58f63572c1643e3467889
philippembambi <philippembambi413@gmail.com>
2024-01-09 15:52:22 +0100

    add lg responsiveness in Help page

--- src/components/Pages/Help/index.js
+++ src/components/Pages/Help/index.js
@@ -6,11 +6,15 @@ import { StaticImage } from "gatsby-plugin-image";
 const Help = () => {
   return (
     <Layout pageTitle={"Page d'aide"}>
-      <section className="mb-7">
+      <section className="mb-7 md:hidden">
         <div className="grid grid-cols-12 gap-2">
           <div className="col-span-5">
             <div className={styles.ellipse__first}>
-              <StaticImage alt="tourist" height={150} src="../../../images/Free-Photo-Tourist-runningfast.png" />
+              <StaticImage
+                alt="tourist"
+                height={150}
+                src="../../../images/Free-Photo-Tourist-runningfast.png"
+              />
             </div>
             <div className={styles.ellipse__second}>
               <StaticImage
@@ -32,7 +36,10 @@ const Help = () => {
             </p>
           </div>
           <div className="col-span-2">
-            <StaticImage alt="plane" src="../../../images/earth-with-plane-drawing-png.png" />
+            <StaticImage
+              alt="plane"
+              src="../../../images/earth-with-plane-drawing-png.png"
+            />
           </div>
         </div>
 
@@ -91,6 +98,90 @@ const Help = () => {
           />
 
           <p className="text-sky-600 font-extrabold text-lg my-5">Suivant </p>
+        </div>
+      </section>
+
+      <section className="mb-7 hidden md:block">
+        <div className="grid grid-cols-12 mt-60">
+          <div className="col-span-5">
+            <div className={styles.ellipse__first}>
+              <StaticImage
+                alt="tourist"
+                height={450}
+                src="../../../images/Free-Photo-Tourist-runningfast.png"
+              />
+            </div>
+            <div className={styles.ellipse__second}>
+              <StaticImage
+                height={450}
+                alt="plane"
+                src="../../../images/png-clipart-black-airway-aviation-aircraft-removebg-preview.png"
+              />
+            </div>
+          </div>
+          <div className="col-span-5 text-sky-600 text-5xl font-extrabold z-10 -mt-20 -ml-40">
+            Dites-nous en quoi pouvons-nous vous aider ?
+          </div>
+          <div className="col-span-2">
+            <StaticImage
+              alt="plane"
+              src="../../../images/earth-with-plane-drawing-png.png"
+            />
+          </div>
+        </div>
+
+        <div className="bg-[#862304] h-80 w-full mt-7 text-center text-white text-xl px-44 py-20">
+          Nous n'offrons malheureusement pas des bourses! Nous accompagnons ceux
+          qui souhaitent poursuivre leurs études supérieures à l'étranger par
+          leurs propres moyens. Vous trouverez toutes les offres des bourses
+          dont nous avons connaissance sur notre site web rdcetudes.com/bourses
+          <p className="mt-2">
+            <button className={styles.help__btn}>En savoir plus</button>
+          </p>
+        </div>
+
+        <div className="py-10 mx-60 text-center text-2xl">
+          L'accaompagnement RDC ETUDES consiste à vous orienter dans votre
+          projet d'études du choix de l'université jusqu'à l'obtention de votre
+          visa.
+        </div>
+
+        <div className="border rounded-lg px-10 mx-72 py-7 shadow-md">
+          <p className="uppercase text-center text-xl">
+            Dans quel pays voulez-vous étudier ?{" "}
+          </p>
+
+          <div className="inline-block relative w-full py-5">
+            <select className="block appearance-none w-full h-16 bg-white border-2 border-black hover:border-gray-500 px-4 py-2 pr-8 rounded-lg shadow leading-tight focus:outline-none focus:shadow-outline">
+              <option disabled selected hidden className="text-gray-300">
+                Choisissez un pays
+              </option>
+              <option>Canada</option>
+              <option>USA</option>
+              <option></option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
+
+          <p className="uppercase text-center text-lg">
+            Avez-vous déjà obtenu votre admission ?{" "}
+          </p>
+
+          <input
+            type="text"
+            placeholder="Oui ou Non"
+            className="mt-5 block h-16 appearance-none w-full bg-white border-2 border-black hover:border-gray-500 px-4 py-2 pr-8 rounded-lg shadow leading-tight focus:outline-none focus:shadow-outline"
+          />
+
+          <p className="text-sky-600 font-extrabold text-xl my-5">Suivant </p>
         </div>
       </section>
     </Layout>
